7c7bbd861fbc43d5f1cf44765d2ea2b2e3e90bdf
Роман <roman-li-83@yandex.ru>
2025-04-10 23:51:09 +0500

    define ROOT, ROOT_MODULE

--- Constants.php
+++ Constants.php
@@ -6,4 +6,9 @@ class Constants
     const string SECTION_PUBLIC = "public";
     const string SECTION_ADMIN = "admin";
     const string SECTION_LK ="lk";
+
+
 }
+defined('ROOT') or define('ROOT','./app');
+defined('ROOT_MODULE') or define('ROOT_MODULE','./app/modules');
+
