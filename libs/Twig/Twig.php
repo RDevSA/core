4aca2bf85bb82b2e64359b7d96752785de00cb34
Роман <roman-li-83@yandex.ru>
2025-04-14 23:50:47 +0500

    cleanup code

--- libs/Twig/Twig.php
+++ libs/Twig/Twig.php
@@ -9,6 +9,25 @@ class Twig
 {
     private static array $prepend;
 
+    public function init():void
+    {
+        $twig = new Environment(self::setTwigLoader(), [
+            'cache' => $_SERVER['DOCUMENT_ROOT'].'core/cache',
+            'auto_reload' => true,
+        ]);
+        $template = $twig->load('index.html.twig');
+        echo $template->render();
+    }
+
+    public static function setTwigLoader():FilesystemLoader
+    {
+        $loader = new FilesystemLoader(ROOT);
+        foreach (self::getPrepend() as $template) {
+            $loader->prependPath($template,'public');
+        }
+        return $loader;
+    }
+
     public static function getPrepend(): array
     {
         return self::$prepend;
@@ -19,34 +38,5 @@ class Twig
         $moduleTemplate = ROOT_MODULE.'/module_'.$path.'/view/html';
         self::$prepend[] = $moduleTemplate;
     }
-
-    public static function setTwigLoader():FilesystemLoader
-    {
-        $moduleTemplates = ROOT_MODULE.'/module_header/view/html';
-
-        $loader = new FilesystemLoader(ROOT);
-        //$loader->prependPath($moduleTemplates,'public');
-        foreach (self::getPrepend() as $template) {
-            $loader->prependPath($template,'public');
-        }
-        return $loader;
-    }
-
-
-
-    public function init():void
-    {
-
-
-        //print_r(self::getPrepend());
-        //self::setTwigLoader()->prependPath(ROOT_MODULE.'/module_header/view/html','public');
-        $twig = new Environment(self::setTwigLoader(), [
-            'cache' => $_SERVER['DOCUMENT_ROOT'].'core/cache',
-            'auto_reload' => true,
-        ]);
-        $template = $twig->load('index.html.twig');
-        echo $template->render();
-    }
-
 
 }
