4db5f737eae3b9de323715a797192a7a3b05c120
Роман <roman-li-83@yandex.ru>
2025-04-15 23:25:44 +0500

    replace 'const' to 'define'

--- Constants.php
+++ Constants.php
@@ -9,6 +9,10 @@ class Constants
 
 
 }
+defined('SECTION_PUBLIC')or define('SECTION_PUBLIC','public');
+defined('SECTION_ADMIN')or define('SECTION_ADMIN','admin');
+defined('SECTION_LK')or define('SECTION_LK','lk');
+
 defined('ROOT') or define('ROOT','./app');
 defined('ROOT_MODULE') or define('ROOT_MODULE','./app/modules');
 
