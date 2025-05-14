f75965cfbb24f8faba2747cfcaf0939a6cc88ce3
Роман <roman-li-83@yandex.ru>
2025-05-14 23:50:56 +0500

    add functions setPrepend() and init()

--- libs/Twig/v2_Twig.php
+++ libs/Twig/v2_Twig.php
@@ -2,7 +2,28 @@
 
 namespace Core\Libs\Twig;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class v2_Twig
 {
+
+    private static function setPrepend(): string
+    {
+        return APP_ROOT;
+    }
+    public function init()
+    {
+        $loader = new FilesystemLoader(APP_ROOT);
+        //$loader->addPath(self::setPrepend());
+        $twig = new Environment($loader);
+
+        $template = $twig->load('test.html.twig');
+        echo $template->renderBlock('title');
+
+        //echo $twig->render('index.html.twig');
+    }
+
+
 
 }
