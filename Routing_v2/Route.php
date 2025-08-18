54aa201383823424977329cebcb360eea81c4cb2
Роман <roman-li-83@yandex.ru>
2025-08-18 23:26:34 +0500

    add constructor of class

--- Routing_v2/Route.php
+++ Routing_v2/Route.php
@@ -1,7 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Core\Routing_v2;
 
 class Route {
-    
+
+    public function __construct(
+        private string $name,
+        private string $path,
+        private array $params,
+        private array $methods=['GET']
+    )
+    {
+        if ($this->methods === []){
+            throw new \InvalidArgumentException('HTTP methods argument was empty; must contain at least one method');
+        }
+    }
+
+
 }
