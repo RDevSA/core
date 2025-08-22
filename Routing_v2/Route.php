c1141fcd1bb65ff147ee6823488967fd9a826af6
Roman <roman_usmga@mail.ru>
2025-08-22 09:58:50 +0500

    add getters for vars $name, $path, $params, $methods

--- Routing_v2/Route.php
+++ Routing_v2/Route.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Core\Routing_v2;
 
-class Route {
+class Route
+{
 
     private $vars = [];
 
@@ -22,34 +23,62 @@ class Route {
         private string $name,
         private string $path,
         private array $params,
-        private array $methods=['GET']
-    )
-    {
-        if ($this->methods === []){
+        private array $methods = ['GET']
+    ) {
+        if ($this->methods === []) {
             throw new \InvalidArgumentException('HTTP methods argument was empty; must contain at least one method');
         }
     }
 
-    public function match(string $path, string $method):bool
+    public function match(string $path, string $method): bool
     {
         $regex = $this->getPath();
-        foreach ($this->getVarsNames() as $variable){
-            $varName = trim($variable,'{\}');
-            $regex = str_replace($variable,'(?P<' . $varName . '>[^/]++)', $regex);
+        foreach ($this->getVarsNames() as $variable) {
+            $varName = trim($variable, '{\}');
+            $regex = str_replace($variable, '(?P<' . $varName . '>[^/]++)', $regex);
         }
 
-        if(in_array($method,$this->getMethods())&&preg_match('^#'.$regex.'$#sD',self::trimPath($path),$matches)){
-            $values = array_filter($matches,static function($key){
+        if (in_array($method, $this->getMethods()) && preg_match('^#' . $regex . '$#sD', self::trimPath($path), $matches)) {
+            $values = array_filter($matches, static function ($key) {
                 return is_string($key);
-            },ARRAY_FILTER_USE_KEY);
+            }, ARRAY_FILTER_USE_KEY);
             foreach ($values as $key => $value) {
-                $this->vars[$key]=$value;
+                $this->vars[$key] = $value;
             }
             return true;
         }
         return false;
     }
-    
 
+        /**
+         * Get the value of name
+         */ 
+        public function getName():string
+        {
+                return $this->name;
+        }
 
+        /**
+         * Get the value of path
+         */ 
+        public function getPath():string
+        {
+                return $this->path;
+        }
+
+        /**
+         * Get the value of params
+         */ 
+        public function getParams():array
+        {
+                return $this->params;
+        }
+
+        /**
+         * Get the value of methods
+         */ 
+        public function getMethods():array
+        {
+                return $this->methods;
+        }
 }
