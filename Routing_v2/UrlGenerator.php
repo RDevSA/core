fa8638a77df04fe2e34995ec333f8461ff4c0baa
Roman <roman_usmga@mail.ru>
2025-08-22 16:03:32 +0500

    create fun generate()

--- Routing_v2/UrlGenerator.php
+++ Routing_v2/UrlGenerator.php
@@ -4,8 +4,26 @@ declare(strict_types=1);
 
 namespace Core\Routing_v2;
 
+use InvalidArgumentException;
+
 class UrlGenerator
 {
 
     public function __construct(private \ArrayAccess $routes) {}
+
+    public function generate(string $name, array $params = []): string
+    {
+        if ($this->routes->offsetExists($name) === false) {
+            throw new InvalidArgumentException(sprintf('Unknown %s name route', $name));
+        }
+        $route = $this->routes[$name];
+
+        if ($route->hasVars() && $params === []) {
+            throw new InvalidArgumentException(sprintf('%s route need parameters: %s', $name, implode(',', $route->getVarsNames())));
+        }
+
+        return self::resolveUri($route, $params);
+    }
+
+
 }
