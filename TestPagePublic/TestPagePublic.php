6df8c7cdcd34fd80fa64fcce77addb418c473674
Roman <roman_usmga@mail.ru>
2025-08-26 15:03:38 +0500

    add funs getAll() and get()

--- TestPagePublic/TestPagePublic.php
+++ TestPagePublic/TestPagePublic.php
@@ -4,5 +4,17 @@ declare(strict_types = 1);
 namespace Core\TestPagePublic;
 
 class TestPagePublic {
-    
+    public function getAll()
+    {
+        return json_encode([
+            ['id' => 1],
+            ['id' => 2],
+            ['id' => 3]
+        ]);
+    }
+
+    public function get($id) 
+    {
+        return json_encode(['id' => $id]);
+    }
 }
