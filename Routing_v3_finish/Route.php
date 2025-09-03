14519b367757b8320b8706ec5500ecf2a3de4c92
Roman <roman_usmga@mail.ru>
2025-09-03 16:55:41 +0500

    add class Route (Core\Routing_v3_finish)

---
+++ Routing_v3_finish/Route.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace Core\Routing_v3_finish;
+
+class Route {
+    public function __construct()
+    {
+        print('Routing_v3_finish include!');
+    }
+}
