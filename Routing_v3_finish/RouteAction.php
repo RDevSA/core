abef57c980d64c4bfdabb284668943f6e34253df
Roman <roman_usmga@mail.ru>
2025-09-08 10:58:51 +0500

    add func matchFromPath(), init()

--- Routing_v3_finish/RouteAction.php
+++ Routing_v3_finish/RouteAction.php
@@ -3,8 +3,9 @@ declare(strict_types=1);
 namespace Core\Routing_v3_finish;
 
 class RouteAction {
-    public function __construct()
+    public function init()
     {
-        
+        $route = new Router();
+        $route->matchFromPath($_SERVER['HTTP_HOST']);
     }
 }
--- Routing_v3_finish/Router.php
+++ Routing_v3_finish/Router.php
@@ -5,11 +5,14 @@ namespace Core\Routing_v3_finish;
 class Router {
     public function __construct()
     {
-        var_dump('Routing_v3_finish include!');
-        var_dump($_SERVER['HTTP_HOST']);
+        //var_dump('Routing_v3_finish include!');
+        //var_dump($_SERVER['HTTP_HOST']);
     }
 
-    private function matchFromPath($host){
-
+    public function matchFromPath($host){
+        $app_section = explode('.',$host);
+        $length = count($app_section);
+        var_dump('app_section = ',$app_section,$length);
+        //$page ? explode('/', $page) : ['main'];
     }
 }
