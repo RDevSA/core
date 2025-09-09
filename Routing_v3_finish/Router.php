75fedd295e73a6771a10482e4117fe34e0eb11ed
Роман <roman-li-83@yandex.ru>
2025-09-09 23:39:40 +0500

    add test fun  parseUrl()

--- Routing_v3_finish/Router.php
+++ Routing_v3_finish/Router.php
@@ -15,4 +15,17 @@ class Router {
         var_dump('app_section = ',$app_section,$length);
         //$page ? explode('/', $page) : ['main'];
     }
+
+    public function parseUrl()
+    {
+        $url =                      'https://garden.na4u.ru/';
+        $url_admin =                'https://admin.garden.na4u.ru/';
+        $url_dev =                  'https://dev.garden.na4u.ru/';
+        $url_admin_dev =            'https://admin.dev.garden.na4u.ru/';
+        $url_prod =                 'https://garden.ru/';
+        $url_admin_prod =           'https://admin.garden.ru/';
+        $url_admin_dev_prod =       'https://admin.dev.garden.ru/';
+
+
+    }
 }
