15f61147abb1d785f8ae4a6fdbb58fcbbc004b5c
Roman <roman_usmga@mail.ru>
2025-09-11 11:38:57 +0500

    begin develop fun fromArray()

---
+++ Utils.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core;
+
+class Utils
+{
+
+    public static function fromArray(array $data = [])
+    {
+        foreach (get_object_vars($obj = new self) as $property => $default) {
+            if (!array_key_exists($property, $data)) continue;
+            $obj->{$property} = $data[$property]; // assign value to object
+        }
+        return $obj;
+    }
+}
