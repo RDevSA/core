0eb1a5fdb5dbe8c5e0bedb5a4b65f45670e9d020
Roman <roman_usmga@mail.ru>
2025-09-15 16:47:00 +0500

    replace  function parseUrl()

--- Routing_v3_finish/Middleware/Parser.php
+++ Routing_v3_finish/Middleware/Parser.php
@@ -4,5 +4,18 @@ declare(strict_types=1);
 namespace Core\Routing_v3_finish\Middleware;
 
 class Parser {
-    
+
+    public function parseUrl()
+    {
+        $url =                      'https://garden.na4u.ru/';
+        $url_admin =                'https://admin.garden.na4u.ru/';
+        $url_dev =                  'https://dev.garden.na4u.ru/';
+        $url_admin_dev =            'https://admin.dev.garden.na4u.ru/';
+        $url_prod =                 'https://garden.ru/';
+        $url_admin_prod =           'https://admin.garden.ru/';
+        $url_admin_dev_prod =       'https://admin.dev.garden.ru/';
+
+
+    }
+
 }
