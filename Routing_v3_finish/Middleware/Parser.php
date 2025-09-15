1e01f246f60904191a6a6dcf7decdbb5941fbd39
Roman <roman_usmga@mail.ru>
2025-09-15 17:00:02 +0500

    delete vars from fun parseUrl() to __construct

--- Routing_v3_finish/Middleware/Parser.php
+++ Routing_v3_finish/Middleware/Parser.php
@@ -1,21 +1,23 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Core\Routing_v3_finish\Middleware;
 
-class Parser {
+class Parser
+{
 
-    public function parseUrl()
-    {
-        $url =                      'https://garden.na4u.ru/';
-        $url_admin =                'https://admin.garden.na4u.ru/';
-        $url_dev =                  'https://dev.garden.na4u.ru/';
-        $url_admin_dev =            'https://admin.dev.garden.na4u.ru/';
-        $url_prod =                 'https://garden.ru/';
-        $url_admin_prod =           'https://admin.garden.ru/';
-        $url_admin_dev_prod =       'https://admin.dev.garden.ru/';
+    public function __construct(
+        private    $url =                      'https://garden.na4u.ru/',
+        private    $url_admin =                'https://admin.garden.na4u.ru/',
+        private    $url_dev =                  'https://dev.garden.na4u.ru/',
+        private    $url_admin_dev =            'https://admin.dev.garden.na4u.ru/',
+        private    $url_prod =                 'https://garden.ru/',
+        private    $url_admin_prod =           'https://admin.garden.ru/',
+        private    $url_admin_dev_prod =       'https://admin.dev.garden.ru/'
+    ) {}
 
+    public function isTestServer() {}
 
-    }
-
+    
 }
