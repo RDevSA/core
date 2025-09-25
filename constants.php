fcbd37a1e4d51ed093ef589f7cebab49496663ae
Роман <roman-li-83@yandex.ru>
2025-09-25 05:54:31 +0500

    add constants

--- constants.php
+++ constants.php
@@ -10,3 +10,8 @@ defined('APP_ROOT_MODULE') or define('APP_ROOT_MODULE','./app/modules');
 defined('CONFIG_PAGES') or define('CONFIG_PAGES',APP_ROOT.'/config_pages.php');
 
 defined('PAGE_COMPONENTS') or define('PAGE_COMPONENTS', ['header','menu']);
+
+
+defined('CONFIG_APP_SECTION') or define('CONFIG_APP_SECTION','app_section');
+defined('CONFIG_APP_MODE') or define('CONFIG_APP_MODE','app_mode');
+defined('CONFIG_TEST_DOMAIN') or define('CONFIG_TEST_DOMAIN','test_domain');
