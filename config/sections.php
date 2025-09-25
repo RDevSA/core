ca226bde146fc05543b49c3128080b998df8ffed
Роман <roman-li-83@yandex.ru>
2025-09-25 05:55:44 +0500

    app_sections => app_section

--- config/sections.php
+++ config/sections.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'app_sections'=>['public','admin','lk'],
+    'app_section'=>['public','admin','lk'],
     'app_mode'=>['dev'],
     'test_domain'=>['na4u'],
     'domain'=>['ru','рф'],
