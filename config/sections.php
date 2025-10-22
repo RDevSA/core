da8cf0b4462f0afbf75c26e2d326f20a6890e75b
Roman <roman_usmga@mail.ru>
2025-10-22 16:42:42 +0500

    add domains for testing

--- config/sections.php
+++ config/sections.php
@@ -3,6 +3,6 @@
 return [
     'app_section'=>['public','admin','lk'],
     'app_mode'=>['dev'],
-    'test_domain'=>['na4u'],
+    'test_domain'=>['na4u','na5u','na6u'],
     'domain'=>['ru','рф'],
 ];
